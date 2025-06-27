802e6ab04fd5e7a53c8ecd2c3891dcfd5a74b00b
Dabeey <abbasdaughter18@gmail.com>
2025-06-27 16:32:05 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var imgBox = document.querySelector('.img-box');
+var imgWrap = document.querySelector('.img-wrap');
+
+var leftSpace = imgBox.offsetLeft;
+
+
+imgBox.onmousemove = function(e) {
+    var boxWidth = (e.pageX - leftSpace) + 'px';
+    imgWrap.computedStyleMap.width = boxWidth;
+}
